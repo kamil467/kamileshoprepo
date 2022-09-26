255ff2659b379de6a044a45448b0a783e4d4aee9
kamil.hussain <kamil.hussain@qburst.com>
2022-09-26 19:54:26 +0530

    API versioning implemented

---
+++ KamilCataLogAPI/Extensions/CustomAPIVersion.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace KamilCataLogAPI.Extensions
+{
+    /// <summary>
+    /// CustomAPIVersion class.
+    /// </summary>
+    public static class CustomAPIVersion
+    {
+        /// <summary>
+        /// Adding custome API version extension method of Iservice interface.
+        /// </summary>
+        /// <param name="services">service collection objects.</param>
+        public static void AddCustomAPIVersion(this IServiceCollection services)
+        {
+            services.AddApiVersioning(o =>
+            {
+                //add information to response headers(api-supported-version,api-deprecated-version)
+                // which version is supported and which going to deprecate
+                o.ReportApiVersions = true; // in our case we will write to response as api-supported-version:1.0 
+
+                //our API shoule be backward compatability even if customer not mentioning API version number. 
+                o.AssumeDefaultVersionWhenUnspecified = false;
+                // false - it will block all the urls without version.( this will block even separate route defined) 
+                // true - this will allow uirls without version - but not working - we have to define route for it. THIS IS A BUG.
+
+
+                o.DefaultApiVersion = new ApiVersion(1, 0); // this is the first versioning of our API. No need to specify at controller
+                                                            // if we set here.
+                                                            // It is a good idea to set AssumDefaultVersionWhenUnSpecified = false
+                                                            // and define default route for Headertypes sources and url version  route.
+                o.ApiVersionReader = ApiVersionReader.Combine(
+                    new QueryStringApiVersionReader("api-version"), // reading from query string
+                    new HeaderApiVersionReader("Accept-Version"), // reading from header
+                    new MediaTypeApiVersionReader("api-version"), // reading from mediatype
+                    new UrlSegmentApiVersionReader()  // reading from url
+                    );
+
+            });
+        }
+    }
+}
